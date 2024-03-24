c973b3b28868739c6633fc42e4a0276f9e306677
Justin Lang <jlang61@ucsb.edu>
2024-03-23 18:50:53 -0700

    adding initial changes to mailing

---
+++ client/data/project.ts
@@ -0,0 +1,14 @@
+const ProjectInfo = [
+    {
+        name: "Code & Grow",
+        description: " ", 
+        imageRef: " ",
+    },
+    {
+        name: "UCSB TASA Website",
+        description: " ",
+        imageRef: " ",
+    }
+]
+
+export default ProjectInfo
